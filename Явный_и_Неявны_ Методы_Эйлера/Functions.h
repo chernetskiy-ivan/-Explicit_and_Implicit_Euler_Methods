9a5c50d6633a4740e18652e9fa58a16384fe2104
Ivan <ivanchernetskey@mail.ru>
2020-11-20 18:08:18 +0300

    add files which contains Function

---
+++ Явный_и_Неявны_ Методы_Эйлера/Functions.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<iostream>
+#include<math.h>
+
+double* Function(double* vector_F, double* vector_U);
---
+++ Явный_и_Неявны_ Методы_Эйлера/Явный_и_Неявны_ Методы_Эйлера.cpp
@@ -0,0 +1,21 @@
+﻿#include"Functions.h"
+
+using namespace std;
+
+double eps = 0.001;
+double u[2] = { 1,0 };
+int T = 1;
+double tmax = 0.1;
+
+void main() {
+	setlocale(LC_ALL,"ru");
+	cout << "Все работает норм. Кчау" << endl;
+	
+	double res[2];
+	Function(res, u);
+
+	for (int i = 0; i < 2; i++) {
+		cout << res[i] << endl;
+	}
+
+}
